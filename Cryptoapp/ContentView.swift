bc5af85b6a3346d7bfcb9e9626200c3690db2916
Carlos Alexandro <pokebol7771@gmail.com>
2025-06-11 19:36:17 -0600

    Initial Commit

---
+++ Cryptoapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cryptoapp
+//
+//  Created by Carlos López García on 11/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
